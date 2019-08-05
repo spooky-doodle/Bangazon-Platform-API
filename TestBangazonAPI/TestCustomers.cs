ef32c2e53f00669ccf0f2a0844431a149f19b236
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-05 11:53:06 -0500

    Wrote failing unit test

--- TestBangazonAPI/TestCustomers.cs
+++ TestBangazonAPI/TestCustomers.cs
@@ -36,5 +36,32 @@ namespace TestBangazonAPI
                 Assert.True(customers.Count > 0);
             }
         }
+
+        [Fact]
+        public async Task Test_Get_One_Customer()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/customers/1");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customer = JsonConvert.DeserializeObject<Customer>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(customer.FirstName != null);
+            }
+        }
     }
 }
