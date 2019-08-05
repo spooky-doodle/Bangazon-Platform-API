c1015d07b30be57a254e71a3f4920f28b466bb71
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-05 15:34:11 -0500

    Implemented all models

--- BangazonAPI/Models/Order.cs
+++ BangazonAPI/Models/Order.cs
@@ -1,21 +1,21 @@
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BangazonAPI.Models
 {
-    public class Customer
+    public class Order
     {
         public int Id { get; set; }
-
         [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
+        public int CustomerId { get; set; }
 
         public List<Product> Products { get; set; }
 
-        public PaymentType PaymentType { get; set; }
+        public Customer Customer { get; set; }
+
 
     }
 }
