227c7eeb037c557f11b7e4ab82d6245bd020d05e
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-05 13:31:14 -0500

    Wrote failing unit test

--- TestBangazonAPI/TestCustomers.cs
+++ TestBangazonAPI/TestCustomers.cs
@@ -5,6 +5,8 @@ using Xunit;
 using BangazonAPI.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 
 namespace TestBangazonAPI
 {
@@ -61,6 +63,46 @@ namespace TestBangazonAPI
                 */
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 Assert.True(customer.FirstName != null);
+                Assert.True(customer.Id == 1);
+            }
+        }
+        [Fact]
+        public async Task Test_Create_New_Customer()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+                Customer newCustomer = new Customer()
+                {
+                    FirstName = "Bob",
+                    LastName = "Barker"
+                };
+
+                var jsonCustomer = JsonConvert.SerializeObject(newCustomer);
+
+                /*
+                    ACT
+                */
+                var response = await client.PostAsync(
+                    "/api/customers",
+                    new StringContent(jsonCustomer, Encoding.UTF8, "application/json")
+                    );
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customer = JsonConvert.DeserializeObject<Customer>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.True(customer.Id != 0);
+                Assert.Equal(customer.FirstName, newCustomer.FirstName);
+                Assert.Equal(customer.LastName, newCustomer.LastName);
+
+
             }
         }
     }
