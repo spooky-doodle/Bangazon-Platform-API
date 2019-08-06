2ae7c413a330edcc51bbd9bafb26292fad8f3700
Nate Fleming <nfleming4@gmail.com>
2019-08-06 11:48:02 -0700

    get all returns orders with products embedded

--- BangazonAPI/Controllers/OrdersControllers.cs
+++ BangazonAPI/Controllers/OrdersControllers.cs
@@ -43,51 +43,50 @@ namespace BangazonAPI.Controllers
                 {
                     cmd.CommandText = @"SELECT o.Id, o.CustomerId, o.PaymentTypeId, p.CustomerId AS PCId, p.[Description], p.Id AS ProductId, p.Price, p.ProductTypeId, p.Quantity, p.Title
                                         FROM [Order] o
-                                        JOIN OrderProduct op ON op.OrderId = o.Id
-                                        JOIN Product p ON p.Id = op.ProductId";
+                                        LEFT JOIN OrderProduct op ON op.OrderId = o.Id
+                                        LEFT JOIN Product p ON p.Id = op.ProductId";
                     SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
                     List<Order> orders = new List<Order>();
 
                     while (reader.Read())
                     {
+                        Order order = new Order
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            Products = new List<Product>()
+                        };
                         try
                         {
-                            Order order = new Order
-                            {
-                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
-                                PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
-                                //Products = new List<Product>()
-                            };
-
-                            orders.Add(order);
+                            order.PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId"));
+                
                         }
                         catch (SqlNullValueException)
                         {
-                            return null;
+                            // Don't do anything
                         }
 
-                        //Product product = new Product
-                        //{
-                        //    Id = reader.GetInt32(reader.GetOrdinal("ProductId")),
-                        //    ProductTypeId = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
-                        //    CustomerId = reader.GetInt32(reader.GetOrdinal("PCId")),
-                        //    Price = reader.GetDecimal(reader.GetOrdinal("Price")),
-                        //    Title = reader.GetString(reader.GetOrdinal("Title")),
-                        //    Description = reader.GetString(reader.GetOrdinal("Description")),                       
-                        //};
+                        Product product = new Product
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("ProductId")),
+                            ProductTypeId = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("PCId")),
+                            Price = reader.GetDecimal(reader.GetOrdinal("Price")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                        };
 
-                        //if (orders.Any(o => o.Id == order.Id))
-                        //{
-                        //    Order existingOrder = orders.Find(o => o.Id == order.Id);
-                        //    existingOrder.Products.Add(product);
-                        //}
-                        //else
-                        //{
-                        //    order.Products.Add(product);
-                        //    orders.Add(order);
-                        //}
+                        if (orders.Any(o => o.Id == order.Id))
+                        {
+                            Order existingOrder = orders.Find(o => o.Id == order.Id);
+                            existingOrder.Products.Add(product);
+                        }
+                        else
+                        {
+                            order.Products.Add(product);
+                            orders.Add(order);
+                        }
                     }
 
                     reader.Close();
