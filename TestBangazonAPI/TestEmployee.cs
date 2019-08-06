ae3d871d80b14c63fade5dbcd236706af6b48116
Colin Sandlin <csandlin010@gmail.com>
2019-08-06 10:59:56 -0700

    wrote failing test for GetAllEmployees

---
+++ TestBangazonAPI/TestEmployee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TestBangazonAPI
+{
+    public class TestEmployee
+    {
+        [Fact]
+        public async Task Test_Get_All_PaymentTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                 * ARRANGE
+                 */
+
+                /*
+                * ACT
+                */
+                var response = await client.GetAsync("/api/Employees");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var employees = JsonConvert.DeserializeObject<List<Employee>>(responseBody);
+
+                /*
+                * ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(employees.Count > 0);
+            }
+        }
+
+    }
+}
