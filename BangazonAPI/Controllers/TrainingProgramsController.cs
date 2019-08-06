7e2c66c25788b95e9d010b0eda8127fd568f1c9d
godspeedless@gmail.com <godspeedless@gmail.com>
2019-08-06 13:02:12 -0700

    Pause for PR -incomplete work

---
+++ BangazonAPI/Controllers/TrainingProgramsController.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainingProgramsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public TrainingProgramsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, [Name], StartDate, EndDate, MaxAttendees FROM TrainingProgram";
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    List<TrainingProgram> trainingPrograms = new List<TrainingProgram>();
+                    while (await reader.ReadAsync())
+                    {
+                        TrainingProgram trainingProgram = new TrainingProgram
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("NAme")),
+                            StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                            EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))                          
+                        };
+
+                        trainingPrograms.Add(trainingProgram);
+                    }
+
+                    reader.Close();
+
+                    return Ok(trainingPrograms);
+                }
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, [Name], StartDate, EndDate, MaxAttendees 
+                                        FROM TrainingProgram 
+                                        WHERE p.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    TrainingProgram trainingProgram = null;
+                    if (reader.Read())
+                    {
+                        trainingProgram = new TrainingProgram
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("NAme")),
+                            StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                            EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees"))
+                            // You might have more columns
+                        };
+                    }
+
+                    reader.Close();
+
+                    return Ok(product);
+                }
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Product product)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = @"
+                        INSERT INTO Product (ProductTypeId, CustomerId, Title, Description, Quantity, Price)
+                        OUTPUT INSERTED.Id
+                        VALUES (@ProductTypeId, @CustomerId, @Title, @Description, @Quantity, @Price)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@ProductTypeId", product.ProductTypeId));
+                    cmd.Parameters.Add(new SqlParameter("@CustomerId", product.CustomerId));
+                    cmd.Parameters.Add(new SqlParameter("@Title", product.Title));
+                    cmd.Parameters.Add(new SqlParameter("@Description", product.Description));
+                    cmd.Parameters.Add(new SqlParameter("@Quantity", product.Quantity));
+                    cmd.Parameters.Add(new SqlParameter("@Price", product.Price));
+
+                    product.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+                }
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute]int id, [FromBody] Product product)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            UPDATE Product
+                            SET ProductTypeId = @ProductTypeId,
+                                CustomerId = @CustomerId,
+                                Title = @Title,
+                                Description = @Description,
+                                Quantity = @Quantity,
+                                Price = @Price       
+                            WHERE Id = @id
+                        ";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@ProductTypeId", product.ProductTypeId));
+                        cmd.Parameters.Add(new SqlParameter("@CustomerId", product.CustomerId));
+                        cmd.Parameters.Add(new SqlParameter("@Title", product.Title));
+                        cmd.Parameters.Add(new SqlParameter("@Description", product.Description));
+                        cmd.Parameters.Add(new SqlParameter("@Quantity", product.Quantity));
+                        cmd.Parameters.Add(new SqlParameter("@Price", product.Price));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        //DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute]int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "DELETE FROM Product WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool ProductExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = "SELECT Id FROM Product WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ TestBangazonAPI/TestTrainingPrograms.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+
+namespace TestBangazonAPI
+{
+    public class TestTrainingPrograms
+    {
+        [Fact]
+        public async Task Test_Get_All_Training_Programs()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/trainingPrograms");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<List<Product>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(products.Count > 0);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Get_One_Training_Program()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/trainingPrograms/1");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var trainingProgram = JsonConvert.DeserializeObject<TrainingProgram>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(trainingProgram.Name != null);
+                Assert.True(trainingProgram.Id == 1);
+            }
+        }
+        [Fact]
+        public async Task Test_Create_New_Training_Program()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+                TrainingProgram newTrainingProgram = new TrainingProgram()
+                {
+                    Name = "Test Training Program",
+                    StartDate = new DateTime(2019, 08, 26),
+                    EndDate = new DateTime(2019, 09, 15),
+                    MaxAttendees = 20
+                };
+
+                var jsonTrainingProgram = JsonConvert.SerializeObject(newTrainingProgram);
+
+                /*
+                    ACT
+                */
+                var response = await client.PostAsync(
+                    "/api/trainingPrograms",
+                    new StringContent(jsonTrainingProgram, Encoding.UTF8, "application/json")
+                    );
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var trainingProgram = JsonConvert.DeserializeObject<TrainingProgram>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.True(trainingProgram.Id != 0);
+                Assert.Equal(trainingProgram.Name, newTrainingProgram.Name);
+                Assert.Equal(trainingProgram.MaxAttendees, newTrainingProgram.MaxAttendees);
+
+
+            }
+        }
+
+        [Fact]
+        public async Task Test_Update_Existing_Training_Program()
+        {
+            int testId = 1;
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+                TrainingProgram testTrainingProgram = new TrainingProgram()
+                {
+                    Name = "Test Training Program 2",
+                    StartDate = new DateTime(2020, 08, 26),
+                    EndDate = new DateTime(2020, 09, 15),
+                    MaxAttendees = 25
+                };
+
+                var jsonTrainingPrgram = JsonConvert.SerializeObject(testTrainingProgram);
+
+                /*
+                    ACT
+                */
+                var response = await client.PutAsync(
+                    $"/api/trainingProgram/{testId}",
+                    new StringContent(jsonTrainingPrgram, Encoding.UTF8, "application/json")
+                    );
+
+
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+                /*   
+                 *   GET   
+                */
+
+                var getTrainingPrograms = await client.GetAsync($"/api/trainingPrograms/{testId}");
+                getTrainingPrograms.EnsureSuccessStatusCode();
+
+                string getResponse = await getTrainingPrograms.Content.ReadAsStringAsync();
+                var updatedTrainingProgram = JsonConvert.DeserializeObject<TrainingProgram>(getResponse);
+
+                Assert.Equal(HttpStatusCode.OK, getTrainingPrograms.StatusCode);
+                Assert.Equal(testId, updatedTrainingProgram.Id);
+                Assert.Equal(testTrainingProgram.Name, updatedTrainingProgram.Name);
+                Assert.Equal(testTrainingProgram.MaxAttendees, updatedTrainingProgram.MaxAttendees);
+
+
+            }
+        }
+
+        [Fact]
+        public async Task Test_Update_Nonexisting_Training_Program()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+                TrainingProgram testTrainingProgram = new TrainingProgram()
+                {
+                    Name = "Test Training Program 2",
+                    StartDate = new DateTime(2020, 08, 26),
+                    EndDate = new DateTime(2020, 09, 15),
+                    MaxAttendees = 25
+                };
+
+                var jsonTrainingProgram = JsonConvert.SerializeObject(testTrainingProgram);
+
+                /*
+                    ACT
+                */
+                var response = await client.PutAsync(
+                    "/api/trainingPrograms/9999999",
+                    new StringContent(jsonTrainingProgram, Encoding.UTF8, "application/json")
+                    );
+
+
+
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+
+            }
+        }
+
+        [Fact]
+        public async Task Test_Delete_Existing_Future_Training_Programs()
+        {
+
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+                TrainingProgram newTrainingProgram = new TrainingProgram()
+                {
+                    Name = "Test Training Program 2",
+                    StartDate = new DateTime(2020, 08, 26),
+                    EndDate = new DateTime(2020, 09, 15),
+                    MaxAttendees = 25
+                };
+
+                var jsonTrainingProgram = JsonConvert.SerializeObject(newTrainingProgram);
+
+
+                var response = await client.PostAsync(
+                    "/api/products",
+                    new StringContent(jsonTrainingProgram, Encoding.UTF8, "application/json")
+                    );
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var trainingProgram = JsonConvert.DeserializeObject<TrainingProgram>(responseBody);
+
+
+                /*
+                    ACT
+                */
+                var deleteResponse = await client.DeleteAsync($"/api/trainingPrograms/{trainingProgram.Id}");
+
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Delete_Existing_Past_Training_Programs()
+        {
+
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+         
+                /*
+                    ACT
+                */
+                var deleteResponse = await client.DeleteAsync($"/api/trainingPrograms/1");
+
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.Forbidden, deleteResponse.StatusCode);
+            }
+        }
+
+
+        [Fact]
+        public async Task Test_Delete_Nonexisting_Product()
+        {
+
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+                /*
+                    ACT
+                */
+                var deleteResponse = await client.DeleteAsync($"/api/traingingPrograms/135123233");
+
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.NotFound, deleteResponse.StatusCode);
+
+
+
+
+
+            }
+        }
+    }
+}
