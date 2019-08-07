47fb7ad656c5987eb84d318447ed89fec5e30a64
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-07 14:20:26 -0500

    Updated get one order test to check for embedded products

--- TestBangazonAPI/TestOrders.cs
+++ TestBangazonAPI/TestOrders.cs
@@ -151,8 +151,10 @@ namespace TestBangazonAPI
             }
         }
 
+        
+
         [Fact]
-        public async Task Test_Get_Single_Order()
+        public async Task Test_Get_One_Order()
         {
             using (var client = new APIClientProvider().Client)
             {
@@ -165,38 +167,6 @@ namespace TestBangazonAPI
                     ACT
                 */
                 var response = await client.GetAsync("/api/orders/1");
-
-
-                string responseBody = await response.Content.ReadAsStringAsync();
-                var order = JsonConvert.DeserializeObject<Order>(responseBody);
-
-                /*
-                 * ASSERT
-                */
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-                Assert.Equal(1, order.Id);
-                Assert.Equal(2, order.CustomerId);
-                Assert.Equal(1, order.PaymentTypeId);
-                Assert.NotNull(order);
-
-
-            }
-        }
-
-        [Fact]
-        public async Task Test_Get_One_Orders_With_Products()
-        {
-            using (var client = new APIClientProvider().Client)
-            {
-                /*
-                    ARRANGE
-                */
-
-
-                /*
-                    ACT
-                */
-                var response = await client.GetAsync("/api/orders/1?_include=products");
 
 
                 string responseBody = await response.Content.ReadAsStringAsync();
